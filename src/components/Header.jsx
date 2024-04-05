65fc4d8b66eb37a197fc5f6d5948195364a420aa
Andarov <bekturandarob678@gmail.com>
2024-04-05 23:39:26 +0500

    Add site header without responsive styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import logo from '../assets/logo.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='py-5'>
+      <div className="w-full max-w-base mx-auto px-5 flex justify-between items-center">
+        {/* logo */}
+        <Link>
+          <img src={logo} alt="Logo" />
+        </Link>
+
+        <div className='flex items-center gap-8'>
+          <nav>
+            <ul className='flex items-center gap-8'>
+              <li>
+                <NavLink to='/' className='text-oast-midnight-300 font-bold hover:text-oast-primary-dark transition-all duration-300'>Auction</NavLink>
+              </li>
+              <li>
+                <NavLink to='/presale' className='text-oast-midnight-300 font-bold hover:text-oast-primary-dark transition-all duration-300'>Join the presale</NavLink>
+              </li>
+              <li>
+                <NavLink to='/faq' className='text-oast-midnight-300 font-bold hover:text-oast-primary-dark transition-all duration-300'>FAQ</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <button className='px-5 py-3.5 bg-oast-primary-medium rounded-xl shadow-btn text-oast-light font-bold border border-[#282b33]'>Connect your wallet</button>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,29 @@ export default {
       colors:{
         oast:{
           main: '#0B0621',
-          primary: '#8951ff',
+          primary:{
+            medium: '#8951ff', 
+            dark: '#A851FF', 
+          },
           midnight:{
             200: '#A8A1C5',
             300: '#6E6889'
-          }
+          },
+          light: '#fbfcff'
         }
+      },
+      boxShadow: {
+        'btn': '0px 1px 2px rgba(24, 39, 75, .04)',
       },
       maxWidth:{
         base: '1280px'
+      },
+      fontFamily:{
+        'dmSans': 'DM Sans, Arial, sans-serif',
+        'montserrat': 'Montserrat, Arial, sans-serif',
       }
     },
   },
   plugins: [],
 }
+
